9b6ddd2f538adca835f4f45b16a9940b9fb6ca63
GitHub Action <action@github.com>
2023-06-17 04:02:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/fairhopeweb/intellijplatformplugindev/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fairhopeweb/intellijplatformplugindev/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.fairhopeweb.intellijplatformplugindev.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fairhopeweb.intellijplatformplugindev.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
